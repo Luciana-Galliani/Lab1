2eeefb8dcbac534b8b622ca96e333c504e12cc89
hp <hp@LAPTOP-P968IMR0.wind3.hub>
2023-03-16 09:29:33 +0100

    v1

--- src/main/java/it/polito/tdp/Lab01/FXMLController.java
+++ src/main/java/it/polito/tdp/Lab01/FXMLController.java
@@ -25,21 +25,63 @@ public class FXMLController {
 
     @FXML
     private Button btnReset;
+    
+    @FXML
+    //aggiungo
+    private Button btnCancella;
 
     @FXML
     private TextField txtParola;
 
     @FXML
     private TextArea txtResult;
+    
+    @FXML
+    //aggiungo
+    private TextArea txtTempo;
 
     @FXML
     void doInsert(ActionEvent event) {
+    	long tempo1= System.nanoTime();
+    	String s="";
+    	String parola= this.txtParola.getText();
+    	elenco.addParola(parola);
+    	for(String str: elenco.getElenco()) {
+    		if(s!="")
+    			s+="\n";
+    		s+= str;
+    	this.txtResult.setText(s);
+    	}
+    	long tempo2= System.nanoTime();
+    	long differenza= tempo2-tempo1;
+    	this.txtTempo.appendText(differenza+"\n");
 
     }
 
     @FXML
     void doReset(ActionEvent event) {
+    	this.txtResult.clear();
+    	this.txtTempo.clear();
+    	this.txtParola.clear();
+    	elenco.reset();
 
+    }
+    
+    @FXML
+    void doCancella(ActionEvent event) {
+    	long tempo1= System.nanoTime();
+    	String s="";
+    	String selezionato= this.txtResult.getSelectedText();
+    	for(String str: elenco.getLista(selezionato)) {
+    		if(s!="")
+    			s+="\n";
+    		s+= str;
+    	this.txtResult.setText(s);
+    	long tempo2= System.nanoTime();
+    	long differenza= tempo2-tempo1;
+    	this.txtTempo.appendText(differenza+"\n");
+    	}
+    	
     }
 
     @FXML
@@ -48,6 +90,8 @@ public class FXMLController {
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCancella != null : "fx:id=\"btnCancella\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTempo != null : "fx:id=\"txtTempo\" was not injected: check your FXML file 'Scene.fxml'.";
         
         elenco = new Parole() ;
     }
--- src/main/java/it/polito/tdp/Lab01/model/Parole.java
+++ src/main/java/it/polito/tdp/Lab01/model/Parole.java
@@ -1,24 +1,43 @@
 package it.polito.tdp.Lab01.model;
 
 import java.util.List;
+import java.util.*;
 
 public class Parole {
+	//attributi
+	LinkedList<String> paroleInserite;
 		
 	public Parole() {
 		//TODO
+		//inizializzo gli attributi
+		this.paroleInserite= new LinkedList<String>();
 	}
 	
 	public void addParola(String p) {
+		paroleInserite.add(p);
 		//TODO
 	}
 	
 	public List<String> getElenco() {
 		//TODO
-		return null;
+		//ordine alfabetico
+		Collections.sort(paroleInserite);
+		return paroleInserite;
 	}
 	
 	public void reset() {
 		// TODO
+		paroleInserite.clear();
 	}
 
+	//creo i metodi
+	
+	//rimuovo la parola che seleziono
+	public List<String> getLista(String a){
+		if(paroleInserite.contains(a))
+			paroleInserite.remove(a);
+		return paroleInserite;
+	}
+	
+	
 }
